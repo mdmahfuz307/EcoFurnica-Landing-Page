e45ae30fa754cb9608d9db6c1ca90f30b03bd910
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2026-01-23 04:40:17 +0600

    Update import paths to include file extensions

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Navbar from '../../components/Navbar';
+import Navbar from '../../components/Navbar.jsx';
 import { Outlet } from 'react-router';
-import Footer from '../../components/Footer';
+import Footer from '../../components/Footer.jsx';
 import Home from '../home/Home';
 
 const Root = () => {
