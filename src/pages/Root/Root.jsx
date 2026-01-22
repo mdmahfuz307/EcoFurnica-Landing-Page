135cb600bcb5a60a6523b6f29feac4a5dc092439
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2026-01-23 04:59:26 +0600

    Fix import path for Home component

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../../components/Navbar.jsx';
 import { Outlet } from 'react-router';
 import Footer from '../../components/Footer.jsx';
-import Home from '../home/Home';
+import Home from '../Home/Home';
 
 const Root = () => {
     return (
